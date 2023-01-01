7f94f994b691598e2ffd820e77e81b4c6b1bfb7e
Harsha Vardhan <x.projekt@outlook.com>
2023-01-01 18:57:59 +0530

    feat: create 4.28.cpp

---
+++ src/4.28.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+
+int main() {
+    std::cout << "bool: " << sizeof (bool) << " bytes" << std::endl;
+
+    std::cout << "char: " << sizeof (char) << " bytes" << std::endl;
+    std::cout << "wchar_t: " << sizeof (wchar_t) << " bytes" << std::endl;
+    std::cout << "char16_t: " << sizeof (char16_t) << " bytes" << std::endl;
+    std::cout << "char32_t: " << sizeof (char32_t) << " bytes" << std::endl;
+
+    std::cout << "short: " << sizeof (short) << " bytes" << std::endl;
+    std::cout << "int: " << sizeof (int) << " bytes" << std::endl;
+    std::cout << "long: " << sizeof (long int) << " bytes" << std::endl;
+    std::cout << "long long: " << sizeof (long long int) << " bytes" << std::endl;
+
+    std::cout << "float: " << sizeof (float) << " bytes" << std::endl;
+    std::cout << "double: " << sizeof (double) << " bytes" << std::endl;
+    std::cout << "long double: " << sizeof (long double) << " bytes" << std::endl;
+}
