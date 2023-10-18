9732e20e9de19a6d4cf2edc850dec21ca37f019e
Harsha Vardhan <x.projekt@outlook.com>
2023-10-18 17:22:10 +0530

    feat: create src/main/cpp/ch1/e1.4.cpp

---
+++ src/main/cpp/ch1/e1.4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Enter two numbers: " << std::endl;
+
+    float x, y;
+    std::cin >> x >> y;
+
+    std::cout << "Product of " << x << " and " << y << " is " << x * y << std::endl;
+
+    return 0;
+}
